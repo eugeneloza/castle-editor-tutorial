0a97169360a1e9b2c4e4002aa89de33ebcf63ed6
eugeneloza <zhloza@gmail.com>
2020-10-04 16:57:06 +0300

    Load 30-size font

--- game/gamefont.pas
+++ game/gamefont.pas
@@ -9,6 +9,7 @@ uses SysUtils,
 
 var
   CartoonFont60: TTextureFont;
+  CartoonFont30: TTextureFont;
 
 procedure LoadFonts;
 
@@ -17,9 +18,11 @@ implementation
 procedure LoadFonts;
 begin
   CartoonFont60 := TTextureFont.Create('castle-data:/fonts/Big_Bottom_Cartoon.ttf', 60, true);
+  CartoonFont30 := TTextureFont.Create('castle-data:/fonts/Big_Bottom_Cartoon.ttf', 30, true);
 end;
 
 finalization
   FreeAndNil(CartoonFont60);
+  FreeAndNil(CartoonFont30);
 end.
 
