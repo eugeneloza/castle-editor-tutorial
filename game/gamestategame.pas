1ce70c0dd34243643faf2c90794dd7122adc7bf6
eugeneloza <zhloza@gmail.com>
2020-10-06 22:02:04 +0300

    QuickButton

--- game/gamestategame.pas
+++ game/gamestategame.pas
@@ -6,7 +6,21 @@ interface
 
 uses
   Classes, SysUtils,
-  CastleUiState, CastleControls;
+  CastleUiState, CastleControls, CastleUiControls, CastleKeysMouse;
+
+type
+  TQuickButton = class(TCastleUserInterface)
+  public
+    OnPress: TNotifyEvent;
+    function Press(const Event: TInputPressRelease): Boolean;
+  end;
+
+type
+  TGamePad = class
+    Group: TCastleUserInterface;
+    Image: TCastleImageControl;
+    Caption: TCastleLabel;
+  end;
 
 type
   TStateGame = class(TUiState)
@@ -36,6 +50,13 @@ begin
   HighScoreLabel := UiOwner.FindRequiredComponent('HighScoreLabel') as TCastleLabel;
   ScoreText.CustomFont := CartoonFont60;
   HighscoreText.CustomFont := CartoonFont30;
+end;
+
+function TQuickButton.Press(const Event: TInputPressRelease): Boolean;
+begin
+  Result := inherited;
+  if Event.EventType = itMouseButton then
+    OnPress;
 end;
 
 end.
