66992533cd6eb9634eb479ee10aac2bcc512aaaf
eugeneloza <zhloza@gmail.com>
2020-10-06 22:19:31 +0300

    Parsing gameplay field

--- game/gamestategame.pas
+++ game/gamestategame.pas
@@ -9,7 +9,7 @@ uses
   CastleUiState, CastleControls, CastleUiControls;
 
 type
-  TGamePad = class
+  TGamePad = record
     Group: TCastleUserInterface;
     Image: TCastleImageControl;
     Caption: TCastleLabel;
@@ -18,6 +18,7 @@ type
 type
   TStateGame = class(TUiState)
   private
+    GamePads: array[1..3, 1..4] of TGamePad;
     ScoreText, ScoreLabel, HighScoreText, HighScoreLabel: TCastleLabel;
   public
     procedure Start; override;
@@ -34,6 +35,7 @@ uses
 procedure TStateGame.Start;
 var
   UiOwner: TComponent;
+  X, Y: Integer;
 begin
   inherited;
   InsertUserInterface('castle-data:/Game.castle-user-interface', FreeAtStop, UiOwner);
@@ -43,6 +45,13 @@ begin
   HighScoreLabel := UiOwner.FindRequiredComponent('HighScoreLabel') as TCastleLabel;
   ScoreText.CustomFont := CartoonFont60;
   HighscoreText.CustomFont := CartoonFont30;
+  for X := 1 to 3 do
+    for Y := 1 to 4 do
+    begin
+      GamePads[X, Y].Group := UiOwner.FindRequiredComponent('ButtonGroup' + Y.ToString + X.ToString) as TCastleUserInterface;
+      GamePads[X, Y].Image := UiOwner.FindRequiredComponent('Button' + Y.ToString + X.ToString) as TCastleImageControl;
+      GamePads[X, Y].Caption := UiOwner.FindRequiredComponent('Label' + Y.ToString + X.ToString) as TCastleLabel;
+    end;
 end;
 
 end.
