a0640c274fff7d8128eb51dddd5cf35522e84a51
eugeneloza <zhloza@gmail.com>
2020-10-10 14:58:44 +0300

    Show corresponding achievements

--- game/gamestateachievements.pas
+++ game/gamestateachievements.pas
@@ -24,7 +24,7 @@ var
 implementation
 uses
   CastleComponentSerialize,
-  CastleSoundEngine,
+  CastleSoundEngine, CastleConfig,
   GameStateMainMenu;
 
 constructor TStateAchievements.Create(AOwner: TComponent);
@@ -42,9 +42,47 @@ end;
 procedure TStateAchievements.Start;
 var
   UiOwner: TComponent;
+  DesignAchievement1, DesignAchievement2, DesignAchievement3,
+    DesignAchievement4, DesignAchievement5: TCastleDesign;
 begin
   inherited;
   InsertUserInterface('castle-data:/Achievements.castle-user-interface', FreeAtStop, UiOwner);
+  if not UserConfig.GetValue('achievement1', false) then
+  begin
+    DesignAchievement1 := UiOwner.FindRequiredComponent('DesignAchievement1') as TCastleDesign;
+    (DesignAchievement1.FindRequiredComponent('AchievementImage') as TCastleImageControl).Image := NoAchievementImage;
+    (DesignAchievement1.FindRequiredComponent('AchievementImage') as TCastleImageControl).OwnsImage := false;
+    (DesignAchievement1.FindRequiredComponent('AchievementDescription') as TCastleLabel).Exists := false;
+  end;
+  if not UserConfig.GetValue('achievement2', false) then
+  begin
+    DesignAchievement2 := UiOwner.FindRequiredComponent('DesignAchievement2') as TCastleDesign;
+    (DesignAchievement2.FindRequiredComponent('AchievementImage') as TCastleImageControl).Image := NoAchievementImage;
+    (DesignAchievement2.FindRequiredComponent('AchievementImage') as TCastleImageControl).OwnsImage := false;
+    (DesignAchievement2.FindRequiredComponent('AchievementDescription') as TCastleLabel).Exists := false;
+  end;
+  if not UserConfig.GetValue('achievement3', false) then
+  begin
+    DesignAchievement3 := UiOwner.FindRequiredComponent('DesignAchievement3') as TCastleDesign;
+    (DesignAchievement3.FindRequiredComponent('AchievementImage') as TCastleImageControl).Image := NoAchievementImage;
+    (DesignAchievement3.FindRequiredComponent('AchievementImage') as TCastleImageControl).OwnsImage := false;
+    (DesignAchievement3.FindRequiredComponent('AchievementDescription') as TCastleLabel).Exists := false;
+  end;
+  if not UserConfig.GetValue('achievement4', false) then
+  begin
+    DesignAchievement4 := UiOwner.FindRequiredComponent('DesignAchievement4') as TCastleDesign;
+    (DesignAchievement4.FindRequiredComponent('AchievementImage') as TCastleImageControl).Image := NoAchievementImage;
+    (DesignAchievement4.FindRequiredComponent('AchievementImage') as TCastleImageControl).OwnsImage := false;
+    (DesignAchievement4.FindRequiredComponent('AchievementDescription') as TCastleLabel).Exists := false;
+  end;
+  if not UserConfig.GetValue('achievement5', false) then
+  begin
+    DesignAchievement5 := UiOwner.FindRequiredComponent('DesignAchievement5') as TCastleDesign;
+    (DesignAchievement5.FindRequiredComponent('AchievementImage') as TCastleImageControl).Image := NoAchievementImage;
+    (DesignAchievement5.FindRequiredComponent('AchievementImage') as TCastleImageControl).OwnsImage := false;
+    (DesignAchievement5.FindRequiredComponent('AchievementDescription') as TCastleLabel).Exists := false;
+  end;
+
 end;
 
 function TStateAchievements.Press(const Event: TInputPressRelease): Boolean;
